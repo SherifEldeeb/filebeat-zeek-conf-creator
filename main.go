b37f782cdf124c6eb266aaca30ab9445168629ff
Sherif Eldeeb <archeldeeb@gmail.com>
2019-12-13 00:22:44 +0300

    init

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import "fmt"
+
+var logs = []string{
+	"conn",
+	"dce_rpc",
+	"dhcp",
+	"dnp3",
+	"dns",
+	"ftp",
+	"http",
+	"irc",
+	"kerberos",
+	"modbus",
+	"modbus_register_change",
+	"mysql",
+	"ntlm",
+	"radius",
+	"rdp",
+	"rfb",
+	"sip",
+	"smb_cmd",
+	"smb_files",
+	"smb_mapping",
+	"smtp",
+	"snmp",
+	"socks",
+	"ssh",
+	"ssl",
+	"syslog",
+	"tunnel",
+	"files",
+	"ocsp",
+	"pe",
+	"x509",
+	"netcontrol",
+	"netcontrol_drop",
+	"netcontrol_shunt",
+	"netcontrol_catch_release",
+	"openflow",
+	"intel",
+	"notice",
+	"notice_alarm",
+	"signatures",
+	"traceroute",
+	"known_certs",
+	"known_hosts",
+	"known_modbus",
+	"known_services",
+	"software",
+	"barnyard2",
+	"dpd",
+	"unified2",
+	"weird",
+}
+
+var conf = `filebeat.inputs:
+%s
+output.kafka:
+  enabled: true
+  hosts: ["localhost:9092"]
+  topic: zeek
+  codec.json:
+    pretty: false
+    escape_html: false
+`
+
+var log = `- type: log
+  enabled: true
+  paths:
+    - /opt/zeek/logs/current/%[1]s.log
+  json.keys_under_root: true
+  fields_under_root: true
+  fields.logtype: zeek-%[1]s
+`
+
+func main() {
+	var AllLogs string
+	for _, l := range logs {
+		AllLogs = AllLogs + fmt.Sprintf(log, l)
+	}
+	fmt.Println(fmt.Sprintf(conf, AllLogs))
+}
